c8bc4d8fe5531222aa6328ffb30e81fbbd537c7d
grund <peter.grund@bisnode.com>
2023-07-07 11:15:54 +0200

    optional expanding vars, minor help improivements

--- krokimagic/krokimagic.py
+++ krokimagic/krokimagic.py
@@ -43,16 +43,20 @@ class KrokiMagic(Magics):
     
     @cell_magic
     @magic_arguments()
-    @argument('type', type=DiagramType, help=f'kroki diagram type, valid types: {DiagramType._member_names_}')
-    @argument('--url', '-u', help="URL of kroki server to be used", default=os.getenv('KROKI_ENDPOINT', "https://kroki.io"))
-    @argument('--ssl', action=BooleanOptionalAction, help="verify ssl chaon", default=True)
+    @argument('type', type=DiagramType, help=f'required kroki diagram type, one of {DiagramType._member_names_}')
+    @argument('--url', '-u', help="URL of kroki server to be used, defaults to os.env['KROKI_ENDPOINT'] and if not set to https://kroki.io]", default=os.getenv('KROKI_ENDPOINT', "https://kroki.io"))
+    @argument('--ssl', action=BooleanOptionalAction, help="verify ssl chain", default=True)
+    @argument('--expand_vars', action=BooleanOptionalAction, metavar="--expand-vars", help="replace/expand variables from user namespace within cell content before sending to kroki", default=False)
     def kroki(self, line, cell):
         
+        
         args = parse_argstring(self.kroki, line)
+
+        content= cell.format(**self.shell.user_ns) if args.expand_vars else cell
         
         if not args.ssl:
             requests.packages.urllib3.disable_warnings() 
-        r = requests.post(f'{args.url}/{args.type}/svg', json={'diagram_source': cell.format(**self.shell.user_ns)}, verify=False)
+        r = requests.post(f'{args.url}/{args.type}/svg', json={'diagram_source': content}, verify=False)
         if r.status_code == 200:
             return display(SVG(r.text))
         else:
